df66d0bf9be7f9537b7098dd99bb9fbb85ab18fa
nick <nick@khadas.com>
2017-08-30 18:25:31 +0800

    VIM2: Env: simplify the default environment(CONFIG_EXTRA_ENV_SETTINGS)

--- board/khadas/configs/kvim2.h
+++ board/khadas/configs/kvim2.h
@@ -46,7 +46,7 @@
 #define CONFIG_CEC_OSD_NAME		"Kvim2"
 #define CONFIG_CEC_WAKEUP
 
-#define CONFIG_INSTABOOT
+//#define CONFIG_INSTABOOT
 
 /* support ext4*/
 #define CONFIG_CMD_EXT4 1
@@ -81,149 +81,34 @@
 /* args/envs */
 #define CONFIG_SYS_MAXARGS  64
 #define CONFIG_EXTRA_ENV_SETTINGS \
-        "firstboot=0\0"\
-        "upgrade_step=0\0"\
-        "jtag=apao\0"\
-        "loadaddr=1080000\0"\
-        "outputmode=1080p60hz\0" \
-        "hdmimode=1080p60hz\0" \
-        "cvbsmode=576cvbs\0" \
-        "display_width=1920\0" \
-        "display_height=1080\0" \
-        "display_bpp=16\0" \
-        "display_color_index=16\0" \
-        "display_layer=osd1\0" \
-        "display_color_fg=0xffff\0" \
-        "display_color_bg=0\0" \
-        "dtb_mem_addr=0x1000000\0" \
-        "fb_addr=0x3d800000\0" \
-        "fb_width=1920\0" \
-        "fb_height=1080\0" \
-        "usb_burning=update 1000\0" \
-        "fdt_high=0x20000000\0"\
-        "try_auto_burn=update 700 750;\0"\
-        "sdcburncfg=aml_sdc_burn.ini\0"\
-        "sdc_burning=sdc_burn ${sdcburncfg}\0"\
-        "wipe_data=successful\0"\
-        "wipe_cache=successful\0"\
-        "EnableSelinux=enforcing\0"\
-        "recovery_part=recovery\0"\
-        "recovery_offset=0\0"\
-        "cvbs_drv=0\0"\
-        "active_slot=_a\0"\
-        "boot_part=boot\0"\
-        "initargs="\
-            "rootfstype=ramfs init=/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xc81004c0 ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 "\
-            "\0"\
-        "upgrade_check="\
-            "echo upgrade_step=${upgrade_step}; "\
-            "if itest ${upgrade_step} == 3; then "\
-                "run init_display; run storeargs; run update;"\
-            "else fi;"\
-            "\0"\
-    "storeargs="\
-            "setenv bootargs ${initargs} androidboot.selinux=${EnableSelinux} logo=${display_layer},loaded,${fb_addr},${outputmode} maxcpus=${maxcpus} vout=${outputmode},enable hdmimode=${hdmimode} cvbsmode=${cvbsmode} hdmitx=${cecconfig} cvbsdrv=${cvbs_drv}  pq=${pq} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
-	"setenv bootargs ${bootargs} androidboot.hardware=amlogic;"\
-            "run cmdline_keys;"\
-            "setenv bootargs ${bootargs} androidboot.slot_suffix=${active_slot};"\
-            "\0"\
-        "switch_bootmode="\
-            "get_rebootmode;"\
-            "if test ${reboot_mode} = factory_reset; then "\
-                    "run recovery_from_flash;"\
-            "else if test ${reboot_mode} = update; then "\
-                    "run update;"\
-            "else if test ${reboot_mode} = cold_boot; then "\
-                /*"run try_auto_burn; "*/\
-            "else if test ${reboot_mode} = fastboot; then "\
-                "fastboot;"\
-            "fi;fi;fi;fi;"\
-            "\0" \
-        "storeboot="\
-            "if imgread kernel ${boot_part} ${loadaddr}; then bootm ${loadaddr}; fi;"\
-            "run update;"\
-            "\0"\
-        "factory_reset_poweroff_protect="\
-            "echo wipe_data=${wipe_data}; echo wipe_cache=${wipe_cache};"\
-            "if test ${wipe_data} = failed; then "\
-                "run init_display; run storeargs;"\
-                "if mmcinfo; then "\
-                    "run recovery_from_sdcard;"\
-                "fi;"\
-                "if usb start 0; then "\
-                    "run recovery_from_udisk;"\
-                "fi;"\
-                "run recovery_from_flash;"\
-            "fi; "\
-            "if test ${wipe_cache} = failed; then "\
-                "run init_display; run storeargs;"\
-                "if mmcinfo; then "\
-                    "run recovery_from_sdcard;"\
-                "fi;"\
-                "if usb start 0; then "\
-                    "run recovery_from_udisk;"\
-                "fi;"\
-                "run recovery_from_flash;"\
-            "fi; \0" \
-         "update="\
-            /*first usb burning, second sdc_burn, third ext-sd autoscr/recovery, last udisk autoscr/recovery*/\
-            "run usb_burning; "\
-            "run sdc_burning; "\
-            "if mmcinfo; then "\
-                "run recovery_from_sdcard;"\
-            "fi;"\
-            "if usb start 0; then "\
-                "run recovery_from_udisk;"\
-            "fi;"\
-            "run recovery_from_flash;"\
-            "\0"\
-        "recovery_from_sdcard="\
-            "setenv bootargs ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if fatload mmc 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;"\
-            "if fatload mmc 0 ${loadaddr} recovery.img; then "\
-                    "if fatload mmc 0 ${dtb_mem_addr} dtb.img; then echo sd dtb.img loaded; fi;"\
-                    "wipeisb; "\
-                    "bootm ${loadaddr};fi;"\
-            "\0"\
-        "recovery_from_udisk="\
-            "setenv bootargs ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if fatload usb 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;"\
-            "if fatload usb 0 ${loadaddr} recovery.img; then "\
-                "if fatload usb 0 ${dtb_mem_addr} dtb.img; then echo udisk dtb.img loaded; fi;"\
-                "wipeisb; "\
-                "bootm ${loadaddr};fi;"\
-            "\0"\
-        "recovery_from_flash="\
-            "setenv bootargs ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if itest ${upgrade_step} == 3; then "\
-                "if ext4load mmc 1:2 ${dtb_mem_addr} /recovery/dtb.img; then echo cache dtb.img loaded; fi;"\
-                "if ext4load mmc 1:2 ${loadaddr} /recovery/recovery.img; then echo cache recovery.img loaded; wipeisb; bootm ${loadaddr}; fi;"\
-            "else fi;"\
-            "if imgread kernel ${recovery_part} ${loadaddr} ${recovery_offset}; then wipeisb; bootm ${loadaddr}; fi;"\
-            "\0"\
-        "init_display="\
-            "osd open;osd clear;imgread pic logo bootup $loadaddr;bmp display $bootup_offset;bmp scale"\
-            "\0"\
-        "cmdline_keys="\
-            "if keyman init 0x1234; then "\
-                "if keyman read usid ${loadaddr} str; then "\
-                    "setenv bootargs ${bootargs} androidboot.serialno=${usid};"\
-                    "setenv serial ${usid};"\
-                "else "\
-                    "setenv bootargs ${bootargs} androidboot.serialno=1234567890;"\
-                    "setenv serial 1234567890;"\
-                "fi;"\
-                "if keyman read mac ${loadaddr} str; then "\
-                    "setenv bootargs ${bootargs} mac=${mac} androidboot.mac=${mac};"\
-                "fi;"\
-                "if keyman read deviceid ${loadaddr} str; then "\
-                    "setenv bootargs ${bootargs} androidboot.deviceid=${deviceid};"\
-                "fi;"\
-            "fi;"\
-            "\0"\
-        "bcb_cmd="\
-            "get_valid_slot;"\
-            "\0"\
+		"boardname=kvim2\0" \
+		"loadaddr=1080000\0" \
+		"dtb_mem_addr=0x1000000\0" \
+		"fdt_high=0x20000000\0" \
+		"outputmode=1080p60hz\0" \
+		"hdmimode=1080p60hz\0" \
+		"display_width=1920\0" \
+		"display_height=1080\0" \
+		"display_bpp=16\0" \
+		"display_color_index=16\0" \
+		"display_layer=osd1\0" \
+		"display_color_fg=0xffff\0" \
+		"display_color_bg=0\0" \
+		"fb_addr=0x3d800000\0" \
+		"fb_width=1920\0" \
+		"fb_height=1080\0" \
+		"init_display=" \
+			"osd open;" \
+			"osd clear;" \
+			"imgread pic logo bootup ${loadaddr};" \
+			"bmp display ${bootup_offset}; bmp scale" \
+		"\0"\
+		"bootdisk=ramdisk\0" \
+		"initargs=" \
+			"root=LABEL=ROOTFS rootflags=data=writeback rw logo=osd1,loaded,0x3d800000,1080p60hz vout=1080p60hz,enable hdmimode=1080p60hz console=ttyS0,115200n8 console=tty0 no_con    sole_suspend consoleblank=0 fsck.repair=yes net.ifnames=0\0" \
+		"storeargs="\
+			"setenv bootargs ${initargs};"\
+		"\0"\
 		"combine_key="\
 			"saradc open 0;"\
 			"if saradc get_in_range 0x0 0x1f; then "\
@@ -249,17 +134,12 @@
 			"\0"\
 
 #define CONFIG_PREBOOT  \
-            "run bcb_cmd; "\
-            "run factory_reset_poweroff_protect;"\
-            "run upgrade_check;"\
             "run init_display;"\
             "run combine_key;" \
 			"run storeargs;"\
             "run upgrade_key;" \
 			"run vim2_check;" \
-			"forceupdate;" \
-            "run switch_bootmode;"
-#define CONFIG_BOOTCOMMAND "run storeboot"
+#define CONFIG_BOOTCOMMAND "ext4load mmc 1:5 1080000 uImage;ext4load mmc 1:5 10000000 uInitrd;ext4load mmc 1:5 20000000 kvim2.dtb;bootm 1080000 10000000 20000000"
 
 //#define CONFIG_ENV_IS_NOWHERE  1
 #define CONFIG_ENV_SIZE   (64*1024)
@@ -470,6 +350,7 @@
 #define CONFIG_FS_FAT 1
 #define CONFIG_FS_EXT4 1
 #define CONFIG_LZO 1
+#define CONFIG_CMD_EXT2 1
 
 /* Cache Definitions */
 //#define CONFIG_SYS_DCACHE_OFF
